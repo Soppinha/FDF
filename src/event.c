417e7ab5bbaf770050a370542bbae182a8e90e3a
Sofia Linux <louisafolkens@gmail.com>
2026-01-16 20:54:40 -0300

    add close

--- src/event.c
+++ src/event.c
@@ -6,7 +6,7 @@
 /*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 17:53:09 by svaladar          #+#    #+#             */
-/*   Updated: 2026/01/14 13:19:38 by svaladar         ###   ########.fr       */
+/*   Updated: 2026/01/16 20:54:00 by svaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,11 @@ int	mouse_zoom(int button, int x, int y, t_fdf *fdf)
 	else if (button == 5 && fdf->cam->scale_factor > 2)
 		fdf->cam->scale_factor -= 2;
 	render(fdf);
+	return (0);
+}
+
+int	close_handle(t_fdf *fdf)
+{
+	close_all(fdf, 0);
 	return (0);
 }
