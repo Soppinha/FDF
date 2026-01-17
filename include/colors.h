8f51ad25cb9748a8ef705d3014f51fc1304fd7f9
Sofia Linux <louisafolkens@gmail.com>
2026-01-16 21:22:25 -0300

    ajujstes na cor

--- include/colors.h
+++ include/colors.h
@@ -6,21 +6,21 @@
 /*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 22:13:59 by svaladar          #+#    #+#             */
-/*   Updated: 2026/01/16 20:30:41 by svaladar         ###   ########.fr       */
+/*   Updated: 2026/01/16 21:21:13 by svaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_H
 # define COLORS_H
 
-# define C_WHITE	0xffffff
-# define C_BLACK	0x000000
+# define C_WHITE		0xffffff
+# define C_BLACK		0x000000
 
-# define C_RED		0xff0000
-# define C_GREEN	0x00ff00
+# define C_RED			0xff0000
+# define C_GREEN		0x00ff00
 # define C_BLUE		0x0000ff
 
-# define C_ORANGE	0xffa500
-# define C_BLUE		0x0492c2
+# define C_ORANGE		0xffa500
+# define C_LIGHT_BLUE	0x0492c2
 
 #endif
--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 22:33:54 by svaladar          #+#    #+#             */
-/*   Updated: 2026/01/16 20:30:44 by svaladar         ###   ########.fr       */
+/*   Updated: 2026/01/16 21:22:03 by svaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static void	apply_colors(t_fdf *fdf, t_point *point)
 		}
 		else
 		{
-			col = color_pallet_init(C_BLACK, C_BLUE);
+			col = color_pallet_init(C_BLACK, C_LIGHT_BLUE);
 			point->color = get_color(col, absolute(point->z),
 					absolute(fdf->map->max_z));
 			free(col);
